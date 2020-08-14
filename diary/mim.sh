b5040d10fee1768789f2f379cdfd60b06433eac8
KeiOguroTs <kei.oguro.ts@mixi.co.jp>
2020-08-14 09:09:46 +0900

    no message

--- diary/mim.sh
+++ diary/mim.sh
@@ -7,7 +7,7 @@ BR=`git branch --show-current`
 git add --all
 git commit -m "no message"
 git checkout master
-git merge --no-ff $BR
+git merge -m "Merge branch '$BR'" --no-ff $BR
 git checkout $BR
 git merge --ff master
 git push --all
