4549b6ca180892ccf7262bb50753d0049bee55d5
Kei Oguro <kei.oguro@thebugmakers.com>
2022-01-28 10:21:21 +0900

    whiteが真っ白だとライトモードで見えないので#c0c0c0にした。同様にダークも#404040にした。

--- tex2svg-gh.sh
+++ tex2svg-gh.sh
@@ -6,13 +6,13 @@ tex2svg1() {
         echo specify output file name.
         exit
     fi
-    if [ "$3" != "" ] ; then
-        tex2svg "$3" > "$1".svg
+    if [ "$4" != "" ] ; then
+        tex2svg "$4" > "$1".svg
     else
         xargs --null tex2svg > "$1".svg < /dev/clipboard
     fi
-    cat "$1".svg | sed s/\\bcurrentColor\\b/"$2"/g > "$1"."$2".svg
+    cat "$1".svg | sed s/\\bcurrentColor\\b/"$3"/g > "$1"."$2".svg
 }
 
-tex2svg1 "$1" black "$2"
-tex2svg1 "$1" white "$2"
+tex2svg1 "$1" black "#404040" "$2"
+tex2svg1 "$1" white "#c0c0c0" "$2"
