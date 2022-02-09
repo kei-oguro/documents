72abaa99544695a39ca789238878fa73ef046488
Kei Oguro <kei.oguro@thebugmakers.com>
2022-02-09 09:54:11 +0900

    commit from 'at-home'

--- tex2svg-gh.sh
+++ tex2svg-gh.sh
@@ -1,20 +1,14 @@
 #! /bin/sh
 
-
-tex2svg1() {
-    if [ "$1" == "" ] ; then
-        echo specify output file name.
-        exit
-    fi
-    tex2svg "$4" > "$1".svg
-    cat "$1".svg | sed s/\\bcurrentColor\\b/"$3"/g > "$1"."$2".svg
-    echo "![]($1.$2.svg#gh-$2-mode-only)"
-}
+if [ "$1" == "" ] ; then
+    echo specify output file name.
+    exit
+fi
 
 EXPRESSION="$2"
 if [ "$EXPRESSION" == "" ] ; then
     EXPRESSION=`cat /dev/clipboard`
 fi
 
-tex2svg1 "$1" light "#404040" "$EXPRESSION"
-tex2svg1 "$1" dark "#c0c0c0" "$EXPRESSION"
+tex2svg "$EXPRESSION" > "$1".svg
+echo "![]($1.svg)"
